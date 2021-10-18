2a5d0f098b7b3a555090f017b74d0fb5ae5fdfb4
Thad <thadisfree@gmail.com>
2021-10-18 10:46:08 -0500

    created exercise 1

--- Program.cs
+++ Program.cs
@@ -4,8 +4,12 @@ namespace OperatorExercise
 {
     class Program
     {
+        
+        
         static void Main(string[] args)
         {
+            //Exercise 1
+            
             Console.WriteLine("Hello World!");
         }
     }
