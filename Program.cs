7c1f3fbfb209b9c9631002889942b47afefb266b
Thad <thadisfree@gmail.com>
2021-10-18 11:59:47 -0500

    another commit

--- Program.cs
+++ Program.cs
@@ -13,20 +13,29 @@ namespace OperatorExercise
             int a = 1;
             int b = 2;
             int c = 5;
+            int d = 5;
             Console.WriteLine(a);
             Console.WriteLine(++a);
             Console.WriteLine(++a);
             Console.WriteLine(++a);
+            
             Console.WriteLine();
             Console.WriteLine(b);
             Console.WriteLine(b++);
             Console.WriteLine(b++);
             Console.WriteLine(b++);
+           
             Console.WriteLine();
-            Console.WriteLine(a);
-            Console.WriteLine(a--);
-            Console.WriteLine(a--);
-            Console.WriteLine(a--);
+            Console.WriteLine(c);
+            Console.WriteLine(c--);
+            Console.WriteLine(c--);
+            Console.WriteLine(c--);
+            
+            Console.WriteLine();
+            Console.WriteLine(d);
+            Console.WriteLine(--d);
+            Console.WriteLine(--d);
+            Console.WriteLine(--d);
 
             Console.ReadKey();
 
